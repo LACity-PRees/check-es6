fa6fbcb70b0be33fd0ed41203dad87c4d401f73f
LACity-PRees <49498464+LACity-PRees@users.noreply.github.com>
2019-04-30 10:55:40 -0700

    Create chkEs6.js

---
+++ chkEs6.js
@@ -0,0 +1,24 @@
+//test es6 compatibility
+function check(){
+	try{
+		//test arrow function (must be es6 compatible browser)
+		new Function("() => {};");
+		//succeeds in es6 compatible browsers
+		return true;
+	}
+	catch(exception){
+		//fails in non es6 browsers
+		return false;
+	}
+}
+
+//check the result
+if(check()){ // if(check()===true)) also works
+	//do all scripts including es6 scripts.
+	document.body.innerHTML += "Compatible";
+}
+//not es6 compatible
+else{
+	//do compatible scripts only
+	document.body.innerHTML += "No";
+}
